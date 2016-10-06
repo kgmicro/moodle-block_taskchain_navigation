605a9d91689a573df75058dd541a109b207f2fd1
Gordon Bateson <gordonbateson@gmail.com>
2016-10-06 23:31:34 +0900

    block_taskchain_navigation complete the functionality to allow filters to be enabled/disabled for activity modules

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 $plugin->component = 'block_taskchain_navigation';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2016091979;
-$plugin->release   = '2016-09-19 (79)';
+$plugin->version   = 2016100680;
+$plugin->release   = '2016-10-06 (80)';
 
 if (defined('ANY_VERSION')) { // Moodle >= 2.2
     $plugin->dependencies = array('mod_taskchain' => ANY_VERSION);
