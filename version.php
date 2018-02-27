2bc7d18816164bd36fe056cc443c800e04b588c9
Gordon Bateson <gordonbateson@gmail.com>
2018-02-27 17:28:43 +0900

    block_taskchain_navigation prevent text color of nav links being overridden by theme

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 $plugin->component = 'block_taskchain_navigation';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2018021209;
-$plugin->release   = '2018-02-12 (09)';
+$plugin->version   = 2018022710;
+$plugin->release   = '2018-02-27 (10)';
 
 if (defined('ANY_VERSION')) { // Moodle >= 2.2
     $plugin->dependencies = array('mod_taskchain' => ANY_VERSION);
