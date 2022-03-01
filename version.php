f45baf740bd386cd7b6abf9cda34986dba557a46
Gordon Bateson <gordonbateson@gmail.com>
2022-03-01 12:29:53 +0900

    block_taskchain_navigation add zebra stripes to the settings page if 'itemslect' checkboxes are used

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 $plugin->component = 'block_taskchain_navigation';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2022022827;
-$plugin->release   = '2022-02-28 (27)';
+$plugin->version   = 2022030128;
+$plugin->release   = '2022-03-01 (28)';
 
 if (defined('ANY_VERSION')) { // Moodle >= 2.2
     $plugin->dependencies = array('mod_taskchain' => ANY_VERSION);
